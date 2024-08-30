b7375119333d9020d8620814870d7b23e872a923
Ishmum <ishmum20000@gmail.com>
2024-08-30 23:05:15 +0800

    Add landing page under construction notice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main>
-      <h1>White Code</h1>
-    </main>
-  );
+	return (
+		<main className="relative">
+			<section className="xl:padding-l wide:padding-r padding-b">
+				<div className="text-2xl w-72 px-4 py-2 mt-10 m-auto">
+					<Image
+						src="/under-construction.avif"
+						alt="Under Construction..."
+						width={0}
+						height={0}
+						sizes="100vw"
+						style={{ width: "100%", height: "auto" }}
+					/>
+				</div>
+			</section>
+		</main>
+	);
 }
