287a87b9d5f3ade4e119b6ab37fa9e197477641e
Ishmum <ishmum20000@gmail.com>
2024-09-01 12:51:28 +0800

    Add footer links

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,22 +1,51 @@
+// Navbar Links
 export const navLinks = [
-  { 
-    name: "Our Work",
-    link: "/work"
-  },
-  {
-    name: "Services",
-    link: "/services",
-  },
-  {
-    name: "About Us",
-    link: "/about"
-  },
-  {
-    name: "News",
-    link: "/news",
-  },
-  {
-    name: "Contact Us",
-    link: "/contact"
-  },
+	{
+		name: "Our Work",
+		link: "/work",
+	},
+	{
+		name: "Services",
+		link: "/services",
+	},
+	{
+		name: "About Us",
+		link: "/about",
+	},
+	{
+		name: "News",
+		link: "/news",
+	},
+	{
+		name: "Contact Us",
+		link: "/contact",
+	},
 ];
+
+// Footer Links
+export const footerLinks = {
+	menu: [
+		{
+			name: "Our Work",
+			link: "/work",
+		},
+		{
+			name: "Services",
+			link: "/services",
+		},
+		{
+			name: "About Us",
+			link: "/about",
+		},
+	],
+	contact: [
+		{
+			name: "News",
+			link: "/news",
+		},
+		{
+			name: "Contact Us",
+			link: "/contact",
+		},
+	],
+};
