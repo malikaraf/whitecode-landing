e58eeab00fe2567610d177fc2e1b9aa743db7a29
Ishmum <ishmum20000@gmail.com>
2024-09-03 02:26:27 +0800

    Set up page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,28 @@
+import {
+	Chat,
+	Discover,
+	Hero,
+	Locations,
+	Mission,
+	Project,
+	Services,
+	Values,
+	Vision,
+} from "@/sections";
 import Image from "next/image";
 
 export default function Home() {
 	return (
 		<main className="relative">
-			<section className="xl:padding-l wide:padding-r padding-b">
-				<div className="text-2xl w-72 px-4 py-2 mt-10 m-auto">
-					<Image
-						src="/under-construction.avif"
-						alt="Under Construction..."
-						width={0}
-						height={0}
-						sizes="100vw"
-						style={{ width: "100%", height: "auto"}}
-					/>
-				</div>
-			</section>
+			<Hero />
+			<Mission />
+			<Project />
+			<Services />
+			<Locations />
+			<Discover />
+			<Vision />
+			<Values />
+			<Chat />
 		</main>
 	);
 }
