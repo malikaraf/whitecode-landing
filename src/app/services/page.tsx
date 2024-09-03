630424ce136ca3d025557c28611fdeb2029e4fab
Ishmum <ishmum20000@gmail.com>
2024-09-04 02:32:27 +0800

    Add all services section

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,10 +1,11 @@
-import { ServicesHero } from '@/sections'
+import { AllServices, ServicesHero } from '@/sections'
 import React from 'react'
 
 const Services = () => {
   return (
     <main>
       <ServicesHero />
+      <AllServices />
     </main>
   )
 }
--- src/sections/services/all-services.tsx
+++ src/sections/services/all-services.tsx
@@ -1,8 +1,11 @@
+import { ItemCard } from '@/components'
 import React from 'react'
 
 const AllServices = () => {
   return (
-    <div>AllServices</div>
+    <div className='w-full h-[70vh] flex justify-center items-center'>
+      <ItemCard title="abc" img="project1-first.png" date='1/1/24' />
+    </div>
   )
 }
 
