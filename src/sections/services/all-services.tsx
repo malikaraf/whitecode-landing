bd834a3a6ddaf18010d9d350c1a7caea003cc15d
Ishmum <ishmum20000@gmail.com>
2024-09-04 12:52:43 +0800

    Add all services

--- src/sections/services/all-services.tsx
+++ src/sections/services/all-services.tsx
@@ -1,11 +1,16 @@
 import { ItemCard } from "@/components";
+import { services } from "@/data";
 import React from "react";
 
 const AllServices = () => {
 	return (
 		<div className="px-2 md:px-6 lg:px-12 py-16 md:py-32 w-full flex items-center justify-center">
 			<div className="w-[100%] md:w-[70vw] flex justify-center items-center gap-5 lg:gap-10 flex-wrap">
-
+        {
+          services.map((service, index) => (
+            <ItemCard key={index} title={service.name} img={service.img} />
+          ))
+        }
 			</div>
 		</div>
 	);
