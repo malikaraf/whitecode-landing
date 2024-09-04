f64f2c71e392cc4b0569e7867de41f2b5a3555ef
Ishmum <ishmum20000@gmail.com>
2024-09-04 13:13:29 +0800

    Add title

--- src/sections/services/all-services.tsx
+++ src/sections/services/all-services.tsx
@@ -4,7 +4,10 @@ import React from "react";
 
 const AllServices = () => {
 	return (
-		<div className="px-2 md:px-6 lg:px-12 py-16 md:py-32 w-full flex items-center justify-center">
+		<div className="px-2 md:px-6 lg:px-12 py-16 md:py-32 w-full flex flex-col items-center justify-center gap-20">
+      <div className="font-jura text-4xl tracking-wide font-[500]">
+        Our Services
+      </div>
 			<div className="w-[100%] md:w-[70vw] flex justify-center items-center gap-5 lg:gap-10 flex-wrap">
         {
           services.map((service, index) => (
