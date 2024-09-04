0eaad81fb23f04d59237ae1b0f13ca59ac790837
Ishmum <ishmum20000@gmail.com>
2024-09-04 12:20:25 +0800

    Update card dimensions

--- src/components/item-card/item-card.tsx
+++ src/components/item-card/item-card.tsx
@@ -1,27 +1,35 @@
 import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
 import React from "react";
 
-const ItemCard = ({title, img, date} : {title: string, img: string, date: string}) => {
+const ItemCard = ({
+	title,
+	img,
+	date,
+}: {
+	title: string;
+	img: string;
+	date?: string;
+}) => {
 	return (
 		<Card
 			shadow="sm"
 			isPressable
 			// onPress={() => console.log("item pressed")}
-			className="px-2 py-3"
+			className="px-2 py-3 w-[324px] max-sm:w-[162px]"
 		>
-			<CardBody className="overflow-hidden p-0 h-[140px]">
+			<CardBody className="overflow-hidden p-0 h-[182px] max-sm:h-[91px]">
 				<Image
 					shadow="sm"
 					radius="lg"
 					width="100%"
 					alt={title}
-					className="w-[248px] object-cover object-top h-auto hover:animate-scroll"
+					className="object-cover object-top h-auto hover:animate-scroll"
 					src={`/${img}`}
 				/>
 			</CardBody>
 			<CardFooter className="text-small justify-between">
-				<b>{title}</b>
-				<p className="text-default-500">{date}</p>
+				<span className="font-montserrat text-lg md:text-xl font-[600]">{title}</span>
+				{date && <p className="text-default-500">{date}</p>}
 			</CardFooter>
 		</Card>
 	);
