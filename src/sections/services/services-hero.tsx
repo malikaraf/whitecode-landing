6e010d0c1b1ed9b3c81b8b94357dbd76803e9755
Ishmum <ishmum20000@gmail.com>
2024-09-04 13:21:43 +0800

    Add animation

--- src/sections/services/services-hero.tsx
+++ src/sections/services/services-hero.tsx
@@ -1,13 +1,20 @@
+"use client";
 import React from "react";
+import { motion } from "framer-motion";
 
 const ServicesHero = () => {
 	return (
 		<div className="bg-black h-[55vh] padding-y px-8 md:px-24 lg:px-28 xl:px-36 flex items-center justify-center mb-2">
-			<div className="w-[90%] xl:w-[70%] text-center">
-				<span className="text-accent font-jura font-[600] text-3xl md:text-4xl tracking-widest">
+			<motion.div
+				initial={{ opacity: 0, y: 20 }} 
+				whileInView={{ opacity: 1, y: 0 }} 
+				transition={{ duration: 0.6 }} 
+				className="w-[90%] xl:w-[70%] text-center"
+			>
+				<span className="text-accent font-jura font-[600] text-3xl md:text-5xl lg:text-6xl tracking-widest">
 					Move beyond integration to intelligence
 				</span>
-			</div>
+			</motion.div>
 		</div>
 	);
 };
