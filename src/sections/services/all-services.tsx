d8c1d04f2cdb693d5a9217b9ea992e9b5ba0172c
Ishmum <ishmum20000@gmail.com>
2024-09-04 13:33:50 +0800

    Add animation

--- src/sections/services/all-services.tsx
+++ src/sections/services/all-services.tsx
@@ -1,20 +1,29 @@
+"use client";
+import { motion } from "framer-motion"
 import { ItemCard } from "@/components";
 import { services } from "@/data";
-import React from "react";
 
 const AllServices = () => {
 	return (
 		<div className="px-2 md:px-6 lg:px-12 py-16 md:py-32 w-full flex flex-col items-center justify-center gap-20">
-      <div className="font-jura text-4xl tracking-wide font-[500]">
+      <motion.div
+      initial={{ opacity: 0, x: 20 }} 
+      whileInView={{ opacity: 1, x: 0 }} 
+      transition={{ duration: 0.6 }} 
+      className="font-jura text-4xl tracking-wide font-[500]">
         Our Services
-      </div>
-			<div className="w-[100%] md:w-[70vw] flex justify-center items-center gap-5 lg:gap-10 flex-wrap">
+      </motion.div>
+			<motion.div
+      initial={{ opacity: 0, x: -20 }} 
+      whileInView={{ opacity: 1, x: 0 }} 
+      transition={{ duration: 0.6 }} 
+      className="w-[100%] md:w-[70vw] flex justify-center items-center gap-5 lg:gap-10 flex-wrap">
         {
           services.map((service, index) => (
             <ItemCard key={index} title={service.name} img={service.img} />
           ))
         }
-			</div>
+			</motion.div>
 		</div>
 	);
 };
