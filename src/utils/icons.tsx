83139f96709b4d9c877304e9649d4b450b5684d8
Ishmum <ishmum20000@gmail.com>
2024-09-05 12:14:50 +0800

    Add right arrow icon

--- src/utils/icons.tsx
+++ src/utils/icons.tsx
@@ -4,10 +4,12 @@ import {
 	FaXTwitter,
 	FaYoutube,
 } from "react-icons/fa6";
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 export const ICONS = {
 	instagram: <FaInstagram />,
 	twitter: <FaXTwitter />,
 	youtube: <FaYoutube />,
 	linkedin: <FaLinkedin />,
+	arrow_right: <FaLongArrowAltRight />
 };
