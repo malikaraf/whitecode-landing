cb8a6248259f6ac21bdea10a9d72ffcf52e3e343
Ishmum <ishmum20000@gmail.com>
2024-09-07 12:58:27 +0800

    Create image carousel component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
 // Export all components from this file
 import ItemCard from "./item-card/item-card";
+import ImageCarousel from "./image-carousel/image-carousel";
 
 export {
   ItemCard,
+  ImageCarousel,
 }
