da1e9aa3e673359d4871fca8d5cd3f637bfa5a1e
Ishmum <ishmum20000@gmail.com>
2024-09-07 13:17:56 +0800

    Update prop name and img src

--- src/components/image-carousel/image-carousel.tsx
+++ src/components/image-carousel/image-carousel.tsx
@@ -6,7 +6,7 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import Image from "next/image";
 
-const ImageCarousel = ({ props }: { props: any }) => {
+const ImageCarousel = ({ projects }: { projects: any }) => {
 	return (
 		<div className="w-full h-[700px]">
 			<Swiper
@@ -23,7 +23,7 @@ const ImageCarousel = ({ props }: { props: any }) => {
 				modules={[Autoplay, Pagination, Navigation]}
 				className="mySwiper w-full h-full"
 			>
-				{props.map((item: any, index: number) => (
+				{projects.map((item: any, index: number) => (
 					<SwiperSlide key={index}>
 						<div className="h-full w-full overflow-hidden relative group">
 							{/* Parent div to constrain height */}
@@ -31,7 +31,7 @@ const ImageCarousel = ({ props }: { props: any }) => {
 								{/* Set a larger height for the image */}
 								<div className="relative h-[2000px] w-full">
 									<Image
-										src={item.img}
+										src={`/${item.img}`}
 										alt={item.name}
 										fill
 										className="object-cover object-top transition-all duration-300 ease-in-out"
