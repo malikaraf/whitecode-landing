ec2911e441dec1715eb4908c388a8ed0eb0731f7
Malik Araf <arafmalik04@gmail.com>
2024-09-07 13:13:25 +0600

    Changes made to the contact page

--- src/sections/contact/addresses.tsx
+++ src/sections/contact/addresses.tsx
@@ -2,8 +2,48 @@ import React from 'react'
 
 const Addresses = () => {
   return (
-    <div>Addresses</div>
-  )
-}
+    <div>
+      <div className="bg-black w-full ">
+      <div className="mx-auto  max-w-[90%] w-auto overflow-hidden ">
+    <p className="text-[64px] mt-[200px] font-montserrat text-white text-center whitespace-nowrap">Where to find Us</p>
+  </div>
+
+  <div className="flex justify-center  space-x-10 mt-[250px] pb-[450px]  ">
+
+  <div className="bg-gray-200 h-[300px] p-6 rounded-lg shadow-lg w-[300px]">
+    <p className="text-black text-xl font-bold font-montserrat">WhiteCode</p>
+    <p className="text-black text-lg font-semibold font-montserrat">(GLOBAL HQ)</p>
+    <p className="text-black text-lg font-medium mt-2 font-montserrat">WhiteCode</p>
+    <p className="text-black text-sm mt-4 font-montserrat">909 Third Avenue</p>
+    <p className="text-black text-sm font-montserrat">New York, New York</p>
+    <p className="text-black text-sm font-montserrat">10022</p>
+    <p className="text-black text-sm font-montserrat">United States</p>
+  </div>
+
+
+  <div className="bg-gray-200 p-6 rounded-lg shadow-lg w-[300px] font-montserrat">
+    <p className="text-black text-xl font-bold">WhiteCode</p>
+    <p className="text-black text-lg font-semibold">(GLOBAL HQ)</p>
+    <p className="text-black text-lg font-medium mt-2">WhiteCode</p>
+    <p className="text-black text-sm mt-4">909 Third Avenue</p>
+    <p className="text-black text-sm">New York, New York</p>
+    <p className="text-black text-sm">10022</p>
+    <p className="text-black text-sm">United States</p>
+  </div>
+
+  <div className="bg-gray-200 p-6 rounded-lg shadow-lg w-[300px] font-montserrat">
+    <p className="text-black text-xl font-bold">WhiteCode</p>
+    <p className="text-black text-lg font-semibold">(GLOBAL HQ)</p>
+    <p className="text-black text-lg font-medium mt-2">WhiteCode</p>
+    <p className="text-black text-sm mt-4">909 Third Avenue</p>
+    <p className="text-black text-sm">New York, New York</p>
+    <p className="text-black text-sm">10022</p>
+    <p className="text-black text-sm">United States</p>
+  </div>
+</div>
+        </div>   
+    </div>
+  );
+};
 
 export default Addresses
