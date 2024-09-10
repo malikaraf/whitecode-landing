4f5ff891e4e2d36212c1602954ad5a055145095f
Ishmum <ishmum20000@gmail.com>
2024-09-10 16:41:42 +0800

    Update responsiveness

--- src/sections/about/about-hero.tsx
+++ src/sections/about/about-hero.tsx
@@ -17,9 +17,9 @@ const AboutHero = () => {
 					<motion.div 
           initial={{opacity: 0, x: -10}}
           whileInView={{opacity: 1, x: 0}}
-          transition={{duration: 0.6,  delay: 0.6}}
+          transition={{duration: 0.6,  delay: 0.3}}
           className="text-accent w-[100%] md:w-[50%] lg:w-[45%] max-md:absolute max-md:z-10 max-md:px-10 max-md:py-20 max-sm:py-10">
-						<span className="font-montserrat text-2xl lg:text-4xl xl:text-5xl max-md:font-[600] max-md:tracking-wide max-md:leading-relaxed">
+						<span className="font-montserrat text-xl lg:text-4xl xl:text-5xl max-md:font-[600] max-md:tracking-wide max-md:leading-relaxed">
 							We are a premier software development firm specializing in
 							creating custom-tailored solutions.
 						</span>
@@ -28,7 +28,7 @@ const AboutHero = () => {
           initial={{opacity: 0, x: 10}}
           whileInView={{opacity: 1, x: 0}}
           transition={{duration: 0.6, delay:0.6}}
-          className="w-[100%] md:w-[50%] lg:w-[45%] xl:w-[40%] z-0"
+          className="w-[100%] md:w-[50%] lg:w-[45%] xl:w-[40%]"
           >
 					<Image
 						src="/about-hero-image.png"
