ce6e8468d732e556c1250cc46086e07a645e15fe
Ishmum <ishmum20000@gmail.com>
2024-09-11 23:38:19 +0800

    Complete the section

--- src/sections/news/news-hero.tsx
+++ src/sections/news/news-hero.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 const NewsHero = () => {
 	return (
-		<div className="h-[55vh] padding-y px-8 md:px-24 lg:px-28 xl:px-36 flex items-center justify-center mb-2">
+		<div className="h-[45vh] md:h-[55vh] padding-y px-8 md:px-24 lg:px-28 xl:px-36 flex flex-col items-center justify-center gap-10">
 			<motion.div
 				initial={{ opacity: 0, y: 20 }}
 				whileInView={{ opacity: 1, y: 0 }}
@@ -12,6 +12,16 @@ const NewsHero = () => {
 			>
 				<span className="font-jura font-[600] text-3xl md:text-5xl lg:text-6xl tracking-widest">
 					Make your dreams realize
+				</span>
+			</motion.div>
+			<motion.div
+				initial={{ opacity: 0, x: -10 }}
+				whileInView={{ opacity: 1, x: 0 }}
+				transition={{ duration: 0.6, delay: 0.3 }}
+				className="w-[100%] md:w-[50%] lg:w-[45%] text-center"
+			>
+				<span className="font-montserrat text-base lg:text-xl xl:text-2xl ">
+					Check out some of our news
 				</span>
 			</motion.div>
 		</div>
