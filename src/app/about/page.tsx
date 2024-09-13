bdbb3c6128dae9bf1b8c079ee958d64ce251f2ba
Ishmum <ishmum20000@gmail.com>
2024-09-13 21:05:28 +0800

    Add about value section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,4 @@
-import { AboutHero, AboutMission, Chat, Story } from '@/sections'
+import { AboutHero, AboutMission, AboutValues, Chat, Story } from '@/sections'
 import React from 'react'
 
 const About = () => {
@@ -7,6 +7,7 @@ const About = () => {
       <AboutHero />
       <AboutMission />
       <Story />
+      <AboutValues />
       <Chat />
     </main>
   )
